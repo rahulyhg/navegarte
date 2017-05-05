a5dc0b38e4d5ae4806fa16ea592089f0594ef02b
Vagner Cardoso <vagnercardosoweb@gmail.com>
2017-05-05 05:22:43 -0300

    - Atualizado [.gitignore] - Atualizado [config/app.php] - Novos metodos para utilização no [Controller.php]

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -13,6 +13,7 @@
 namespace App\Controllers;
 
 use Navegarte\Contracts\BaseController;
+use Slim\Exception\NotFoundException;
 
 /**
  * Class Controller
@@ -25,4 +26,63 @@ use Navegarte\Contracts\BaseController;
  */
 abstract class Controller extends BaseController
 {
+  /**
+   * @param null|string $name
+   *
+   * @return array|mixed
+   */
+  public function param($name = null)
+  {
+    if (is_null($name)) {
+      return $this->request->getParams();
+    }
+    
+    return $this->request->getParam($name);
+  }
+  
+  /**
+   * @param null|string $name
+   * @param array       $data
+   * @param array       $queryParams
+   *
+   * @return \Slim\Http\Response
+   */
+  public function redirect($name = null, $data = [], $queryParams = [])
+  {
+    $name = !is_null($name) ? $name : 'home';
+    
+    return $this->response->withRedirect($this->router()->pathFor($name, $data, $queryParams));
+  }
+  
+  /**
+   * @param mixed $data
+   * @param bool  $aJson
+   * @param int   $status
+   *
+   * @return \Slim\Http\Response
+   */
+  public function json($data, $aJson = false, $status = 200)
+  {
+    if (!empty($aJson)) {
+      $data = [$data];
+    }
+    
+    return $this->response->withJson($data, $status);
+  }
+  
+  /**
+   * @throws \Slim\Exception\NotFoundException
+   */
+  public function notFound()
+  {
+    throw new NotFoundException($this->request, $this->response);
+  }
+  
+  /**
+   * @return \Slim\Router|\Slim\Route
+   */
+  protected function router()
+  {
+    return $this->container['router'];
+  }
 }
--- config/app.php
+++ config/app.php
@@ -7,7 +7,7 @@ return [
    *
    * Define a versão do framework
    */
-  'version' => 'v0.0.2',
+  'version' => 'v0.0.3',
   
   /**
    * App Version
