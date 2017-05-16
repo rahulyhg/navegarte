c885e68eacd4562ba0ed5cf0d5255e2b18dcff73
Vagner Cardoso <vagnercardosoweb@gmail.com>
2017-05-15 21:02:39 -0300

    - Retirado o webpack do mix.

--- webpack.mix.js
+++
@@ -1,9 +0,0 @@
-var mix = require("laravel-mix");
-
-mix
-  .setPublicPath('public')
-  .js('resources/assets/js/app.js', 'public/js')
-  .sass('resources/assets/sass/app.scss', 'public/css', {
-    outputStyle: 'nested'
-  })
-  .version();
