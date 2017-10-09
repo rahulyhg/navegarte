8294317c3b3a368e441094739138cf90bb301dc5
Vagner Cardoso <vagnercardosoweb@gmail.com>
2017-10-09 01:42:20 -0300

    - Adicionado JS para controlar a navegação animada
    - JS para navegação animada quando é lhe passado o data-goto e a DIV data-goto-id que será rolada a página até ela.

---
+++ resources/assets/js/03-NavigationAnimed.js
@@ -0,0 +1,37 @@
+jQuery(function () {
+  var hash = window.location.hash.replace('#', '');
+  
+  /**
+   * Navegação animada dos menus
+   */
+  $('a[data-goto]').on('click', function (event) {
+    event.preventDefault();
+    
+    scrollMenu($(this).attr('data-goto'));
+  });
+  
+  if (hash) {
+    window.history.pushState({}, '', '/');
+  }
+  
+  $(window).load(function () {
+    if (hash) {
+      window.setTimeout(function () {
+        scrollMenu(hash);
+      }, 100);
+    }
+  });
+  
+  /**
+   * Funão para a animação
+   *
+   * @param id
+   */
+  function scrollMenu (id) {
+    var sroll = $('[data-goto-id="' + id + '"]');
+    
+    $('html,body').animate({
+      scrollTop: (sroll.offset().top - 120)
+    }, 500);
+  }
+});
