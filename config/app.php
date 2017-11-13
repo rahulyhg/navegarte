6fb16798d27770ec5c6a85b7a955a3766d98b627
Vagner Cardoso <vagnercardosoweb@gmail.com>
2017-11-13 11:20:48 -0200

    * [ADD] Configuração para ativar ou desativar a sessão.

--- config/app.php
+++ config/app.php
@@ -11,21 +11,21 @@
  */
 
 return [
-    
+
     /**
      * App Version
      *
      * Define a versão do framework
      */
-    'version' => 'v0.1.7',
-    
+    'version' => 'v0.1.8',
+
     /**
      * App Version
      *
      * Define a versão do skeleton
      */
-    'skeleton' => 'v0.1.4',
-    
+    'skeleton' => 'v0.1.5',
+
     /**
      * Encryption app
      *
@@ -36,33 +36,38 @@ return [
         'cipher' => 'AES-256-CBC',
         'regenerate.days' => false
     ],
-    
+
+    /**
+     * Enable and disabled sessions
+     */
+    'session' => false,
+
     /**
      * Application locale configuration
      */
     'locale' => 'pt_BR',
-    
+
     /**
      * App Url
      *
      * Url do sistema
      */
     'url' => (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . str_replace('/index.php', '', $_SERVER['SCRIPT_NAME']),
-    
+
     /**
      * App Maintenance
      *
      * Define se o site vai estar em manutenção
      */
     'maintenance' => false,
-    
+
     /**
      * App Environment
      *
      * Define se o site está em produção ou em desenvolvimento
      */
     'environment' => env('APP_ENV', 'production'),
-    
+
     /**
      * App Timezone
      *
