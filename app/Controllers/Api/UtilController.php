ad28d263b392fd4a354266159aced2ec85b498b8
Vagner Cardoso <vagnercardosoweb@gmail.com>
2018-09-22 17:08:14 -0300

    - Melhoria no retorno de erro da api.

--- app/Controllers/Api/UtilController.php
+++ app/Controllers/Api/UtilController.php
@@ -11,11 +11,11 @@
  */
 
 namespace App\Controllers\Api {
-    
+
     use Core\Contracts\Controller;
     use Core\Helpers\Request;
     use Core\Helpers\Str;
-    
+
     /**
      * Class UtilController
      *
@@ -28,7 +28,7 @@ namespace App\Controllers\Api {
          * @var \Core\Helpers\Request
          */
         protected $curl;
-        
+
         /**
          * Inicia no __construct
          */
@@ -36,7 +36,7 @@ namespace App\Controllers\Api {
         {
             $this->curl = new Request();
         }
-        
+
         /**
          * [GET] /api/util/{method}[/{data:.*}]
          *
@@ -52,10 +52,10 @@ namespace App\Controllers\Api {
             if (!$this->request->isGet()) {
                 throw new \Exception("Verbo HTTP aceito é apenas GET.", E_USER_ERROR);
             }
-            
+
             return $this->createRequest($method, $params);
         }
-        
+
         /**
          * [POST] /api/util/{method}[/{data:.*}]
          *
@@ -71,10 +71,10 @@ namespace App\Controllers\Api {
             if (!$this->request->isPost()) {
                 throw new \Exception("Verbo HTTP aceito é apenas POST.", E_USER_ERROR);
             }
-            
+
             return $this->createRequest($method, $params);
         }
-        
+
         /**
          * [PUT] /api/util/{method}[/{data:.*}]
          *
@@ -90,10 +90,10 @@ namespace App\Controllers\Api {
             if (!$this->request->isPut()) {
                 throw new \Exception("Verbo HTTP aceito é apenas PUT.", E_USER_ERROR);
             }
-            
+
             return $this->createRequest($method, $params);
         }
-        
+
         /**
          * [DELETE] /api/util/{method}[/{data:.*}]
          *
@@ -109,10 +109,10 @@ namespace App\Controllers\Api {
             if (!$this->request->isDelete()) {
                 throw new \Exception("Verbo HTTP aceito é apenas DELETE.", E_USER_ERROR);
             }
-            
+
             return $this->createRequest($method, $params);
         }
-        
+
         /**
          * [GET, POST] /api/util/zipcode/{zipcode}
          *
@@ -130,41 +130,41 @@ namespace App\Controllers\Api {
                 if (empty($params[0])) {
                     throw new \InvalidArgumentException("Você deve passar o CEP para buscar.", E_USER_ERROR);
                 }
-                
+
                 // Verifica se é válido
                 if (strlen(onlyNumber($params[0])) < 8) {
                     throw new \InvalidArgumentException("O CEP {$params[0]} informado deve conter, no mínimo 8 números.", E_USER_ERROR);
                 }
-                
+
                 // Pega o resultado do CEP
                 $result = $this->curl->get("https://viacep.com.br/ws/{$params[0]}/json");
-                
+
                 if (!empty($result['erro'])) {
                     throw new \Exception("O CEP {$params[0]} informado não foi encontrado.", E_USER_ERROR);
                 }
-                
+
                 // Google Maps
                 $mapsParams = [
                     'key' => 'AIzaSyCUiWvcqkPMCH_CgTwbkOp74-9oEHlhMOA',
                     'sensor' => true,
                     'address' => urlencode("{$result['logradouro']} - {$result['bairro']}, {$result['localidade']} - {$result['uf']}, {$result['cep']}, Brasil"),
                 ];
-                
+
                 $maps = $this->curl->get("https://maps.google.com/maps/api/geocode/json", $mapsParams);
-                
+
                 if ($maps['status'] === 'OK' && !empty($maps['results'][0])) {
                     $location = $maps['results'][0]['geometry']['location'];
-                    
+
                     $result['latitude'] = (string) $location['lat'];
                     $result['longitude'] = (string) $location['lng'];
                 }
-                
+
                 return $this->json($result);
             } catch (\Exception $e) {
-                throw new \Exception("[ZIP CODE] {$e->getMessage()}", $e->getCode());
+                throw new \Exception("[ZIP CODE] :: {$e->getMessage()}", $e->getCode());
             }
         }
-        
+
         /**
          * [POST] /api/util/modal-detail
          *
@@ -182,16 +182,16 @@ namespace App\Controllers\Api {
                 if (empty($params['view'])) {
                     throw new \Exception("Você deve passar a view para inserir na modal.", E_USER_ERROR);
                 }
-                
+
                 // Verifica se foi passado o model
                 if (!empty($params['model']) && (!empty($params['id']) && $params['id'] > 0)) {
                     $namespace = "\\App\\Models\\".Str::studly($params['model']);
-                    
+
                     if (!$params['row'] = (new $namespace())->id($params['id'])->limit(1)->get()) {
                         throw new \Exception("Registro não encontrado.", E_USER_ERROR);
                     }
                 }
-                
+
                 // Retorna resultado
                 return $this->json([
                     'object' => [
@@ -199,10 +199,10 @@ namespace App\Controllers\Api {
                     ],
                 ]);
             } catch (\Exception $e) {
-                throw new \Exception("[MODAL DETAIL] {$e->getMessage()}", $e->getCode());
+                throw new \Exception("[MODAL DETAIL] :: {$e->getMessage()}", $e->getCode());
             }
         }
-        
+
         /**
          * Cria a requisição
          *
@@ -215,14 +215,14 @@ namespace App\Controllers\Api {
         {
             $params = array_merge(($params ? explode('/', $params) : []), $this->param());
             $method = Str::camel($method);
-            
+
             try {
                 if (!method_exists($this, $method)) {
                     $method = basename(str_replace('\\', '/', get_class($this)))."::{$method}";
-                    
-                    throw new \Exception(sprintf("Método '%s' não existe.", $method), E_USER_ERROR);
+
+                    throw new \Exception(sprintf("Método (%s) não existe.", $method), E_USER_ERROR);
                 }
-                
+
                 return $this->{$method}($params);
             } catch (\Exception $e) {
                 return $this->json([
