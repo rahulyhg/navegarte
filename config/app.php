9a536f7a6c247d96549cf48a154528715d5a361d
Vagner Cardoso <vagnercardosoweb@gmail.com>
2018-11-13 20:07:38 -0200

    - Nova versão do skeleton.

--- config/app.php
+++ config/app.php
@@ -22,13 +22,13 @@ return [
          * Versão do framework
          */
         
-        'framework' => 'v1.2.5',
+        'framework' => 'v1.2.6',
         
         /**
          * Versão do skeleton
          */
         
-        'skeleton' => 'v1.2.2',
+        'skeleton' => 'v1.2.3',
     
     ],
     
