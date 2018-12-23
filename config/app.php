72b2247e5ac67332ad3c1df69853b1a0680e5370
Vagner Cardoso <vagnercardosoweb@gmail.com>
2018-12-23 01:39:57 -0200

    - Nova versão.

--- config/app.php
+++ config/app.php
@@ -22,13 +22,13 @@ return [
          * Versão do framework
          */
         
-        'framework' => 'v2.0.2',
+        'framework' => 'v2.0.3',
         
         /**
          * Versão do skeleton
          */
         
-        'skeleton' => 'v2.0.2',
+        'skeleton' => 'v2.0.3',
     
     ],
     
