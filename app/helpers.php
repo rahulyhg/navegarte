c4710ce79aed875b793df88dc85916d27164f029
Vagner Cardoso <vagner@navegarte.com.br>
2019-01-02 18:06:22 -0200

    - Melhoria no upload da imagem.

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * VCWeb <https://www.vagnercardosoweb.com.br/>
+ * VCWeb Networks <https://www.vagnercardosoweb.com.br/>
  *
  * @package   VCWeb Networks
  * @author    Vagner Cardoso <vagnercardosoweb@gmail.com>
@@ -33,27 +33,27 @@ if (!function_exists('filter_value')) {
                 $value = '';
             }
         }
-        
+
         if (!empty($value)) {
             switch ($filter) {
                 case 'onlyNumber':
                     $value = onlyNumber($value);
                     break;
-                
+
                 case 'dateDatabase':
                     $value = date('Y-m-d', strtotime(str_replace('/', '-', $value)));
                     break;
-                
+
                 case 'dateTimeDatabase':
                     $value = date('Y-m-d H:i:s', strtotime(str_replace('/', '-', $value)));
                     break;
-                
+
                 case 'moneyDatabase':
                     $value = str_replace(',', '.', str_replace('.', '', $value));
                     break;
             }
         }
-        
+
         return $value;
     }
 }
@@ -75,14 +75,14 @@ if (!function_exists('validate_post')) {
                     $post[$index] = '';
                 }
             }
-            
+
             // Verifica caso esteja preenchido
             if (isset($post[$index]) && (empty($post[$index]) && $post[$index] != '0')) {
                 if (is_string($param)) {
                     throw new InvalidArgumentException($param, E_USER_WARNING);
                 } else {
                     $code = (!empty($param['code']) ? $param['code'] : E_USER_WARNING);
-                    
+
                     throw new InvalidArgumentException($param['message'], $code);
                 }
             }
@@ -146,7 +146,7 @@ if (!function_exists('error_type')) {
         if (is_string($type) && $type !== 'success') {
             $type = E_USER_ERROR;
         }
-        
+
         switch ($type) {
             case E_USER_NOTICE:
             case E_NOTICE:
@@ -164,11 +164,11 @@ if (!function_exists('error_type')) {
             case 'success':
                 $result = 'success';
                 break;
-            
+
             default:
                 $result = 'danger';
         }
-        
+
         return $result;
     }
 }
@@ -190,13 +190,13 @@ if (!function_exists('get_image')) {
     {
         $name = mb_strtoupper($name, 'UTF-8');
         $path = "/fotos/{$table}/{$id}/{$name}";
-        
+
         foreach ([$extension, strtoupper($extension)] as $ext) {
             if ($asset = asset("{$path}.{$ext}", $baseUrl, $version)) {
                 return $asset;
             }
         }
-        
+
         return '';
     }
 }
@@ -217,7 +217,7 @@ if (!function_exists('get_galeria')) {
         $path = ["fotos/{$table}/{$id}/galeria_{$name}", "fotos/fotos_album/{$id}"];
         $array = [];
         $images = [];
-        
+
         // Imagens antigas
         if (file_exists(PUBLIC_FOLDER."/{$path[1]}")) {
             $images = array_values(array_diff(scandir(PUBLIC_FOLDER."/{$path[1]}"), ['.', '..']));
@@ -229,14 +229,14 @@ if (!function_exists('get_galeria')) {
                 $path = "{$path[0]}/";
             }
         }
-        
+
         // Percore as imagens
         foreach ($images as $key => $image) {
             if (preg_match('/(\.jpg|\.jpeg|\.png|\.gif)/i', $image)) {
                 $array[] = "/{$path}%s/{$image}";
             }
         }
-        
+
         return $array;
     }
 }
@@ -265,11 +265,11 @@ if (!function_exists('get_month')) {
             '11' => 'Novembro',
             '12' => 'Dezembro',
         ];
-        
+
         if (array_key_exists($month, $months)) {
             return $months[$month];
         }
-        
+
         return '';
     }
 }
@@ -293,11 +293,11 @@ if (!function_exists('get_day')) {
             '5' => 'Sexta Feira',
             '6' => 'Sábado',
         ];
-        
+
         if (array_key_exists($day, $days)) {
             return $days[$day];
         }
-        
+
         return '';
     }
 }
@@ -307,54 +307,55 @@ if (!function_exists('upload_image')) {
      * Upload de imagem
      *
      * @param array $file
-     * @param string $folder
+     * @param string $directory
      * @param string $name
      * @param int $width
      * @param int $height
+     * @param bool $forceJpg
+     * @param bool $whExact
      *
      * @return array
      * @throws \Exception
      */
-    function upload_image($file, $folder, $name = null, $width = 500, $height = 500)
+    function upload_image($file, $directory, $name = null, $width = 500, $height = 500, $forceJpg = false, $whExact = false)
     {
-        $directory = "/uploads/{$folder}";
-        $extensions = ['jpg', 'png'];
+        $extensions = ['jpg', 'gif', 'png'];
         $images = [];
-        
+
         foreach ($file as $key => $value) {
             $extension = substr(strrchr($value['name'], '.'), 1);
             $name = (empty($name) ? Str::slug(substr($value['name'], 0, strrpos($value['name'], '.'))) : $name);
-            
-            if ($extension == 'jpeg') {
+
+            if ($extension == 'jpeg' || $forceJpg === true) {
                 $extension = 'jpg';
             }
-            
+
             $path = "{$directory}/{$name}.{$extension}";
-            
+
             // Checa extension
             if (!in_array($extension, $extensions)) {
                 throw new \Exception("Apenas as extenções <b>".strtoupper(implode(', ', $extensions))."</b> são aceito para enviar sua imagem.", E_USER_ERROR);
             }
-            
+
             // Checa tamanho
             if (($value['size'] > $max_filesize = get_upload_max_filesize()) || $value['error'] == 1) {
                 throw new \Exception("Sua imagem ultrapassou o limite de tamanho de <b>".Helper::convertBytes($max_filesize)."</b>.", E_USER_ERROR);
             }
-            
+
             // Cria pasta
             if (!file_exists(PUBLIC_FOLDER.$directory)) {
                 mkdir(PUBLIC_FOLDER.$directory, 0755, true);
             }
-            
+
             // Verifica arquivo
             foreach ($extensions as $ext) {
                 $deleted = str_replace(".{$extension}", ".{$ext}", $path);
-                
+
                 if (file_exists(PUBLIC_FOLDER."{$deleted}")) {
                     unlink(PUBLIC_FOLDER."{$deleted}");
                 }
             }
-            
+
             // Verifica se e gif
             if ($extension == 'gif') {
                 if (!move_uploaded_file($value['tmp_name'], PUBLIC_FOLDER.$path)) {
@@ -363,12 +364,22 @@ if (!function_exists('upload_image')) {
             } else {
                 // Tamanho original
                 list($widthOri, $heightOri) = getimagesize($value['tmp_name']);
-                
-                if (!imagem($value['tmp_name'], PUBLIC_FOLDER.$path, ($width > $widthOri ? $widthOri : $width), ($height > $heightOri ? $heightOri : $height), 90)) {
+
+                // Tamanhos calculados
+                $calcWidth = ($width > $widthOri ? $widthOri : $width);
+                $calcHeight = ($height > $heightOri ? $heightOri : $height);
+
+                if ($whExact === true) {
+                    $fnImg = 'imagemTamExato';
+                } else {
+                    $fnImg = 'imagem';
+                }
+
+                if (!$fnImg($value['tmp_name'], PUBLIC_FOLDER.$path, $calcWidth, $calcHeight, 90)) {
                     throw new \Exception("Não foi possível enviar sua imagem, tente novamente em alguns segundos.", E_USER_ERROR);
                 }
             }
-            
+
             $images[] = [
                 'name' => $name,
                 'path' => $path,
@@ -377,7 +388,7 @@ if (!function_exists('upload_image')) {
                 'md5' => md5_file(PUBLIC_FOLDER.$path),
             ];
         }
-        
+
         return $images;
     }
 }
@@ -396,44 +407,44 @@ if (!function_exists('upload_archive')) {
     function upload_archive($file, $folder, $name = null)
     {
         $directory = "/uploads/{$folder}";
-        $extensions = ['zip', 'rar', 'pdf', 'docx', 'jpg', 'png'];
+        $extensions = ['zip', 'rar', 'pdf', 'docx', 'jpg', 'jpeg', 'png', 'gif'];
         $archives = [];
-        
+
         // Envia os arquivos
         foreach ($file as $key => $value) {
             $extension = substr(strrchr($value['name'], '.'), 1);
             $name = Str::slug((empty($name) ? substr($value['name'], 0, strrpos($value['name'], '.')) : $name));
             $path = "{$directory}/{$name}.{$extension}";
-            
+
             // Checa extension
             if (!in_array($extension, $extensions)) {
                 throw new \Exception("Apenas as extenções <b>".strtoupper(implode(', ', $extensions))."</b> são aceito para enviar o arquivo.", E_USER_ERROR);
             }
-            
+
             // Checa tamanho
             if (($value['size'] > $max_filesize = get_upload_max_filesize()) || $value['error'] == 1) {
                 throw new \Exception("Seu arquivo ultrapassou o limite de tamanho de <b>".Helper::convertBytes($max_filesize)."</b>.", E_USER_ERROR);
             }
-            
+
             // Cria pasta
             if (!file_exists(PUBLIC_FOLDER.$directory)) {
                 mkdir(PUBLIC_FOLDER.$directory, 0755, true);
             }
-            
+
             // Verifica arquivo
             foreach ($extensions as $ext) {
                 $deleted = str_replace(".{$extension}", ".{$ext}", $path);
-                
+
                 if (file_exists(PUBLIC_FOLDER.$deleted)) {
                     unlink(PUBLIC_FOLDER.$deleted);
                 }
             }
-            
+
             // Envia arquivo
             if (!move_uploaded_file($value['tmp_name'], PUBLIC_FOLDER.$path)) {
                 throw new \Exception("Não foi possível enviar o arquivo, tente novamente em alguns segundos.", E_USER_ERROR);
             }
-            
+
             $archives[] = [
                 'name' => $name,
                 'path' => $path,
@@ -442,7 +453,7 @@ if (!function_exists('upload_archive')) {
                 'md5' => md5_file(PUBLIC_FOLDER.$path),
             ];
         }
-        
+
         return $archives;
     }
 }
@@ -460,9 +471,9 @@ if (!file_exists('delete_recursive_directory')) {
                 new \RecursiveDirectoryIterator($path),
                 \RecursiveIteratorIterator::CHILD_FIRST
             );
-            
+
             $interator->rewind();
-            
+
             while ($interator->valid()) {
                 if (!$interator->isDot()) {
                     if ($interator->isFile()) {
@@ -471,10 +482,10 @@ if (!file_exists('delete_recursive_directory')) {
                         rmdir($interator->getPathname());
                     }
                 }
-                
+
                 $interator->next();
             }
-            
+
             rmdir($path);
         }
     }
@@ -491,6 +502,10 @@ if (!function_exists('date_for_human')) {
      */
     function date_for_human($dateTime, $precision = 2)
     {
+        if (empty($dateTime)) {
+            return '-';
+        }
+
         // Variáveis
         $minute = 60;
         $hour = 3600;
@@ -500,7 +515,7 @@ if (!function_exists('date_for_human')) {
         $year = 31556926;
         $century = $year * 10;
         $decade = $century * 10;
-        
+
         // Tempos
         $periods = array(
             $decade => array("decada", "decadas"),
@@ -513,10 +528,10 @@ if (!function_exists('date_for_human')) {
             $minute => array("minuto", "minutos"),
             1 => array("segundo", "segundos"),
         );
-        
+
         // Time atual
         $currentTime = (new \DateTime())->getTimestamp();
-        
+
         // Verifica a data passada
         if ($dateTime instanceof \DateTimeInterface) {
             $dateTime = $dateTime->getTimestamp();
@@ -525,41 +540,41 @@ if (!function_exists('date_for_human')) {
         } else {
             $dateTime = str_replace('/', '-', $dateTime);
             $dateTimeCheck = explode('-', explode(' ', $dateTime)[0]);
-            
+
             if (!checkdate($dateTimeCheck[1], $dateTimeCheck[2], $dateTimeCheck[0])) {
                 throw new \InvalidArgumentException("Date passed not valid.", E_USER_ERROR);
             }
-            
+
             $dateTime = (new \DateTime($dateTime))->getTimestamp();
         }
-        
+
         // Quanto tempo já passou da data atual - a data passada
         $passed = $currentTime - $dateTime;
-        
+
         // Monta o resultado
         if ($passed < 5) {
             $output = "5 segundos";
         } else {
             $output = array();
             $exit = 0;
-            
+
             foreach ($periods as $period => $name) {
                 if ($exit >= $precision || $exit > 0 && $period < 1) {
                     break;
                 }
-                
+
                 $result = floor($passed / $period);
-                
+
                 if ($result > 0) {
                     $output[] = $result." ".($result == 1 ? $name[0] : $name[1]);
                     $passed -= $result * $period;
                     $exit++;
                 }
             }
-            
+
             $output = implode(" e ", $output);
         }
-        
+
         return $output;
     }
 }
@@ -576,16 +591,16 @@ if (!function_exists('preg_replace_space')) {
     {
         // Remove comentários
         $string = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $string);
-        
+
         // Remove espaço com mais de um espaço
         $string = preg_replace('/^\s+|\s+$|\r\n|\r|\n|\t|\s+(?=\s)/m', '', $string);
-        
+
         // Remove tag `p` vázia
         $string = preg_replace('/<p[^>]*>[\s\s|&nbsp;]*<\/p>/m', '', $string);
-        
+
         // Remove todas tags vázia
         //$string = preg_replace('/<[\w]*[^>]*>[\s\s|&nbsp;]*<\/[\w]*>/m', '', $string);
-        
+
         return $string;
     }
 }
