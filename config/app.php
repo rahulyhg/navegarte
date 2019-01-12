064c44ec65f325518a847c575800c8af6e0b9917
Vagner Cardoso <vagnercardosoweb@gmail.com>
2019-01-12 01:46:35 -0200

    - Muda a versão para a mais atual.

--- config/app.php
+++ config/app.php
@@ -11,31 +11,31 @@
  */
 
 return [
-    
+
     /**
      * Define a versão da aplicação
      */
-    
+
     'version' => [
-        
+
         /**
          * Versão do framework
          */
-        
-        'framework' => 'v2.0.4',
-        
+
+        'framework' => 'v2.0.6',
+
         /**
          * Versão do skeleton
          */
-        
-        'skeleton' => 'v2.0.4',
-    
+
+        'skeleton' => 'v2.0.5',
+
     ],
-    
+
     /**
      * Registra os serviços
      */
-    
+
     'providers' => [
         \Core\Providers\View\ViewProvider::class,
         \Core\Providers\ErrorProvider::class,
@@ -47,37 +47,37 @@ return [
         \Core\Providers\Jwt\JwtProvider::class,
         \Core\Providers\LoggerProvider::class,
         \Core\Providers\Event\EventProvider::class,
-        
+
         /**
          * Registra serviço da aplicação
          */
-        
+
         \App\Providers\ErrorSlackProvider::class,
     ],
-    
+
     /**
      * Registra as middlewares
      */
-    
+
     'middlewares' => [
-        
+
         /**
          * Middlewares iniciada automática
          */
-        
+
         'automatic' => [
             \Core\Middlewares\GenerateAppKeyMiddleware::class,
             \Core\Middlewares\TrailingSlashMiddleware::class,
             \Core\Middlewares\MaintenanceMiddleware::class,
             \Core\Middlewares\OldInputMiddleware::class,
         ],
-        
+
         /**
          * Middlewares iniciada manual
          */
-        
+
         'manual' => [],
-    
+
     ],
 
 ];
