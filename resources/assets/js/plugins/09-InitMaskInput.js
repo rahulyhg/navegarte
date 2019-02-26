f126d73967b7b7f70960da7d3f5cddfddb5796cf
Vagner Cardoso <vagnercardosoweb@gmail.com>
2019-02-26 11:16:40 -0300

    - Melhorias nos assets e otimizaçoes no ajax.

--- resources/assets/js/plugins/09-InitMaskInput.js
+++ resources/assets/js/plugins/09-InitMaskInput.js
@@ -5,17 +5,16 @@
  */
 
 var initMaskInput = function () {
-  $('.maskYear').mask('0000');
-  $('.maskTime').mask('00:00');
-  $('.maskDate').mask('00/00/0000');
-  $('.maskDateTime').mask('00/00/0000 00:00');
+  $('.maskYear').mask('0000', {placeholder: '____'});
+  $('.maskTime').mask('00:00', {placeholder: '__:__'});
+  $('.maskDate').mask('00/00/0000', {placeholder: '__/__/____'});
+  $('.maskDateTime').mask('00/00/0000 00:00', {placeholder: '__/__/____ __:__'});
   $('.maskMoney').mask('#.##0,00', {reverse: true});
-  $('.maskFloat').mask('#0.00', {reverse: true});
-  $('.maskCNH').mask('#00', {reverse: true});
+  $('.maskFloat').mask('#.##0,00', {reverse: true});
   $('.maskNumber').mask('#00', {reverse: true});
-  $('.maskCpf').mask('000.000.000-00', {reverse: true});
-  $('.maskRg').mask('00.000.000-0', {reverse: true});
-  $('.maskCnpj').mask('00.000.000/0000-00', {reverse: true});
+  $('.maskCpf').mask('000.000.000-00', {reverse: true, placeholder: '___.___.___-__'});
+  $('.maskRg').mask('00.000.000-0', {reverse: true, placeholder: '__.___.___-_'});
+  $('.maskCnpj').mask('00.000.000/0000-00', {reverse: true, placeholder: '__.___.___/____-__'});
   
   $('.maskCep').mask('00000-000', {
     onKeyPress: function (cep, e, field, options) {
@@ -24,6 +23,8 @@ var initMaskInput = function () {
       
       $('.maskCep').mask(mask, options);
     },
+    
+    placeholder: '_____-___',
   });
   
   /**
@@ -35,10 +36,12 @@ var initMaskInput = function () {
     onKeyPress: function (val, e, field, options) {
       field.mask(SPMaskBehavior.apply({}, arguments), options);
     },
+    
+    placeholder: "(__) 9____-____",
   };
   
   $('.maskPhone').mask(SPMaskBehavior, spOptions);
-  $('.maskTelephone').mask('(00) 0000-0000');
+  $('.maskTelephone').mask('(00) 0000-0000', {placeholder: '(__) ____-____'});
 };
 
 /* Carrega o documento */
