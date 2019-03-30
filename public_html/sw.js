f39bd0e26d9faf4ede292631c194007b3c7c241e
Vagner Cardoso <vagnercardosoweb@gmail.com>
2019-03-30 12:15:14 -0300

    - Melhora o sistema de `ServiceWorker` e `manifest` -> `PWA`.

--- public_html/sw.js
+++ public_html/sw.js
@@ -1,86 +1,44 @@
-/**
- * Prototype variáveis
- */
-
-const waitUntil = ExtendableEvent.prototype.waitUntil;
-const respondWith = FetchEvent.prototype.respondWith;
-const promisesMap = new WeakMap();
-
-/**
- * @param promise
- * @return {*|void}
- */
-ExtendableEvent.prototype.waitUntil = function (promise) {
-  const extendableEvent = this;
-  let promises = promisesMap.get(extendableEvent);
-
-  if (promises) {
-    promises.push(Promise.resolve(promise));
-    return;
-  }
-
-  promises = [Promise.resolve(promise)];
-  promisesMap.set(extendableEvent, promises);
-
-  return waitUntil.call(extendableEvent, Promise.resolve().then(function processPromises () {
-    const len = promises.length;
-
-    return Promise.all(promises.map(p => p.catch(() => {}))).then(() => {
-      if (promises.length !== len) {
-        return processPromises();
-      }
-
-      promisesMap.delete(extendableEvent);
-
-      return Promise.all(promises);
-    });
-  }));
-};
-
-/**
- * @param promise
- * @return {any | void}
- */
-FetchEvent.prototype.respondWith = function (promise) {
-  this.waitUntil(promise);
-  return respondWith.call(this, promise);
-};
-
-/**
- * Variávies do sw
- */
-
-const cacheName = 'vcweb-v1';
+// Constants
+const staticCacheName = 'vcweb-v1';
 const filesToCache = [
-  '/manifest.json',
-  '/robots.txt',
-  '/sw.js'
+  '/offline'
 ];
 
-/**
- * Install cache sw
- */
-
-self.addEventListener('install', function (event) {
+// Cache on install
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  
   event.waitUntil(
-    caches.open(cacheName)
-      .then(function (cache) {
+    caches.open(staticCacheName)
+      .then(cache => {
         return cache.addAll(filesToCache);
       })
   );
 });
 
-/**
- * Fetch cache sw
- */
+// Clear cache on activate
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(cacheName => (cacheName.startsWith('vcweb-')))
+          .filter(cacheName => (cacheName !== staticCacheName))
+          .map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+});
 
-self.addEventListener('fetch', function (event) {
-  event.respondWith(caches.match(event.request).then(function (response) {
-    if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
-      return;
-    }
-
-    return response || fetch(event.request)
-      .catch(function (err) {console.error(event.request.url, err);});
-  }));
+// Serve from Cache
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        return caches.match('/offline');
+      })
+  );
 });
